2ce3c6429bb3a9dc1bcef8331a61c36e2a698869
lusia <panfluba@gmail.com>
2022-10-07 00:04:41 +0300

    fix: change position of numbers in donate and make animate burger to cross

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,1 +1,10 @@
- 
+window.onload = function () {
+    let catalog = document.getElementById('button_burger_active');
+    catalog.onclick = function () {
+        if (catalog.classList.contains("active")) {
+            catalog.classList.remove("active");
+        } else {
+            catalog.classList.add("active");
+        }
+    }
+}
