17ba62860a19f25f7f4947e401ececd985aed336
lusia <panfluba@gmail.com>
2022-10-11 00:13:39 +0300

    feat: make a burger menu

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,10 +1,54 @@
+let dark_area = document.getElementsByClassName('dark');
+
 window.onload = function () {
     let catalog = document.getElementById('button_burger_active');
+    // let dark_area = document.getElementsById('dark');
     catalog.onclick = function () {
         if (catalog.classList.contains("active")) {
             catalog.classList.remove("active");
         } else {
             catalog.classList.add("active");
+            dark.classList.add("active");
+
         }
     }
 }
+// window.onload = function () {
+//     let catalog = document.getElementById('button_burger_active');
+//     catalog.onclick = function () {
+//         if (catalog.classList.contains("active")) {
+//             // dark_area.classList.add("active");
+//             catalog.classList.remove("active");
+//         } else {
+//             catalog.classList.add("active");
+
+//         }
+//     }
+
+// }
+
+// window.onload = function () {
+//     let catalog = document.getElementById('button_burger_active');
+//     catalog.onclick = function () {
+//         if (catalog.classList.contains("active")) {
+//             catalog.classList.remove("active");
+//         } else {
+//             catalog.classList.add("active");
+//         }
+//     }
+// }
+
+
+
+
+
+// window.onload = function () {
+//     let catalog = document.getElementById('button_burger_active');
+//     catalog.onclick = function () {
+//         if (catalog.classList.contains("active")) {
+//             catalog.classList.remove("active");
+//         } else {
+//             catalog.classList.add("active");
+//         }
+//     }
+// }
