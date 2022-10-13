7431d4e3e783c7c8f9f70fa440d4adad4c765cce
lusia <panfluba@gmail.com>
2022-10-13 12:07:07 +0300

    feat: add to radiobutton functional

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,4 +1,4 @@
-let dark_area = document.getElementsByClassName('dark');
+// let dark_area = document.getElementsByClassName('dark');
 
 window.onload = function () {
     let catalog = document.getElementById('button_burger_active');
@@ -12,43 +12,5 @@ window.onload = function () {
 
         }
     }
+   
 }
-// window.onload = function () {
-//     let catalog = document.getElementById('button_burger_active');
-//     catalog.onclick = function () {
-//         if (catalog.classList.contains("active")) {
-//             // dark_area.classList.add("active");
-//             catalog.classList.remove("active");
-//         } else {
-//             catalog.classList.add("active");
-
-//         }
-//     }
-
-// }
-
-// window.onload = function () {
-//     let catalog = document.getElementById('button_burger_active');
-//     catalog.onclick = function () {
-//         if (catalog.classList.contains("active")) {
-//             catalog.classList.remove("active");
-//         } else {
-//             catalog.classList.add("active");
-//         }
-//     }
-// }
-
-
-
-
-
-// window.onload = function () {
-//     let catalog = document.getElementById('button_burger_active');
-//     catalog.onclick = function () {
-//         if (catalog.classList.contains("active")) {
-//             catalog.classList.remove("active");
-//         } else {
-//             catalog.classList.add("active");
-//         }
-//     }
-// }
