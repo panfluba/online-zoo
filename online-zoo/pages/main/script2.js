35a04e9cf23043f158cf9b540512be416e5b02a5
lusia <panfluba@gmail.com>
2022-10-13 12:06:23 +0300

    init: add script2.js

---
+++ online-zoo/pages/main/script2.js
@@ -0,0 +1,12 @@
+function changeNumber(val){
+    document.getElementById("another").value =
+    val;
+   }
+
+//    window.onload = function () {
+//     text_value = document.getElementById("another").value;
+//     button_value = document.getElementById('input[type="radio"]').value;
+//     if (text_value ==  button_value){
+//         text_value =  button_value;
+//     }
+//    }
